9987bec982975e7a555a365854ed482846a9e834
balu <balu1072000@gmail.com>
2022-07-08 11:32:36 +0530

    group routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { dataType } from 'user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'grouprout';
+  getData(){
+    const data:dataType={
+    name:'balu',
+    age:20,
+    address:"40 duradundi"
+  }
+  return data
+  
+}
 }
